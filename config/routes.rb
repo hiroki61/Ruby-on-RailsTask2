c595e129dbbbb7914b7aec5ea36d4b16d65cd814
北川弘貴 <hiroki.k@kitagawahiroshitakanoMacBook-puro.local>
2018-01-23 20:52:29 +0900

    Start5

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/top' => 'books#top'
+ 
 
   resources :books
+ # get '/top' => 'books#top'ではなく以下のように記述することでURLがhttp://localhost:3000/topではなくhttp://localhost:3000/でよくなる。
+  root 'books#top'
 end
